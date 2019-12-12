e54945190b9f5d5920facd53290511516fd989d4
defgao <33217367+defgao@users.noreply.github.com>
2019-12-12 13:38:46 +0800

    Create 登陆dluteda.sh

---
+++ 登陆dluteda.sh
@@ -0,0 +1,2 @@
+curl -X POST -H 'Content-Type':'application/x-www-form-urlencoded' -d "action=login&ac_id=3&user_ip=&nas_ip=&user_mac=&url=&username=201792383&password=08044916" "172.20.20.1:801/srun_portal_pc.php?ac_id=3&url
+="
